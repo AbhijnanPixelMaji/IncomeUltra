009f294c5eeae3a18fe6daf9bc8848098662ccc1
AbhijnanPixelMaji <84489890+AbhijnanPixelMaji@users.noreply.github.com>
2025-11-25 18:24:43 +0530

    project setup initial commit

---
+++ IncomeUltra/IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by Abhijnan Maji on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
