9dd9d65785df29301071cca680221bdfeac332d1
AbhijnanPixelMaji <84489890+AbhijnanPixelMaji@users.noreply.github.com>
2025-11-30 09:16:40 +0530

    income picker view

--- IncomeUltra/IncomeUltra/Core/IncomeView.swift
+++ IncomeUltra/IncomeUltra/Core/IncomeView.swift
@@ -21,11 +21,47 @@ private extension IncomeView{
     var incomeView: some View{
         ScrollView{
             VStack(spacing: 16){
-                
+                IncomePickerView($viewModel.incomePickerValue)
+                switch viewModel.incomePickerValue{
+                    case .hourly:
+                   hourlyView
+                case .salary:
+                   salaryView
+                }
             }
+            .padding(.horizontal)
+            .padding(.vertical, 8)
         }
     }
     
+    @ViewBuilder
+    var salaryView: some View{
+       salaryInputView
+       salarayOutputView
+    }
+    @ViewBuilder
+    var hourlyView: some View{
+        hourlyInputView
+        hourlyOutputView
+    }
+    
+    @ViewBuilder
+    var salaryInputView: some View{
+        Text("")
+    }
+    @ViewBuilder
+    var salarayOutputView: some View{
+        Text("")
+    }
+     
+    @ViewBuilder
+    var hourlyInputView: some View{
+        Text("")
+    }
+    @ViewBuilder
+    var hourlyOutputView: some View{
+        Text("")
+    }
     @ToolbarContentBuilder
     var toolbarItem: some ToolbarContent{
         ToolbarItem(placement: .principal){
